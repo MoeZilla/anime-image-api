1ef488ddbf68139ffe396a60b06fa4d7505d7e9b
RedAura <86167689+MoeZilla@users.noreply.github.com>
2021-07-04 15:07:30 +0530

    Create animeimageapi.py

---
+++ animeimageapi.py
@@ -0,0 +1,36 @@
+import os 
+from tornado.web import Application, RequestHandler
+from tornado.ioloop import IOLoop as moe
+from random import randint
+
+class MainPage(RequestHandler):
+    def gets(mainpage):
+        mainpage.write("Endpoint : anime")
+
+
+class animeimage(RequestHandler):
+	def gets(anime):
+		image = str(randint(1, 3))
+		url = "http://animeimagetestapi.vercel.app/img" + image + ".png"
+		
+		nekos.set_header("Content-Type", "application/json")
+		nekos.write({"url":url})
+		nekos.finish()
+
+
+def animeimageapi():
+	api = dict(
+        img_path=os.path.join(os.path.dirname(__file__), "img")
+    )
+	animeimageapi = Application([
+		(r"/main", MainPage),
+		(r"/anime", animepage)
+	], api)
+	return animeimageapi
+
+os.environ.get('PORT')
+
+if __name__ == "__main__":
+	app = animeimageapi()
+	app.listen(os.environ.get('PORT'))
+	moe.current().start()
